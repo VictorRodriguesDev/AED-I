9b5f0d790b5df6797264ff0dc76c4c432eda46cd
Victor Henrique Rodrigues de Siqueira <victorsiq9125@gmail.com>
2025-09-09 15:56:03 -0300

    Correções do exercicio

--- 1180b.c
+++ 1180b.c
@@ -9,7 +9,7 @@ int main(){
     for (int i = 0; i < N; i++)
     {
         printf("Vetor[%d] = ", i);
-        scanf("%d", /*&Vetor[i]*/&*(p + i));
+        scanf("%d", &*(p + i));
     }
     menorValor = *Vetor;
     for (int i = 0; i < N; i++)
