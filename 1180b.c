037eeb141f560048e91ef5ebf55abafa9b3a5a84
Victor Henrique Rodrigues de Siqueira <victorsiq9125@gmail.com>
2025-09-09 13:46:09 -0300

    Exercício de menor valor com vetores, utilizando ponteiros

---
+++ 1180b.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int N, menorValor = 0, posicaoMenor = 0;
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &N);
+    int Vetor[N];
+    for (int i = 0; i < N; i++)
+    {
+        printf("Vetor[%d] = ", i);
+        scanf("%d", &Vetor[i]);
+    }
+    menorValor = *Vetor;
+    for (int i = 0; i < N; i++)
+    {
+        if(*(Vetor + i) < menorValor){
+            menorValor = *(Vetor + i);
+            posicaoMenor = i;
+        }
+    }
+    printf("Menor valor: %d\n", menorValor);
+    printf("Posicao: %d\n", posicaoMenor);
+    return 0;
+}
