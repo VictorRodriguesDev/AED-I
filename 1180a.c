565452b8c0da4f8d69df21d971d9bdc2f5835c62
Victor Henrique Rodrigues de Siqueira <victorsiq9125@gmail.com>
2025-09-10 11:07:04 -0300

    Atualização do arquivo

--- 1180a.c
+++ 1180a.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     int N;
-    int Vetor[N];
-    int menorValor = 9999999, posicaoMenor = 0;
-    printf("Digite o tamanho do vetor: \n");
     scanf("%d", &N);
-    for (int i = 0; i < N; i++)
-    {
-        printf("Vetor[%d] = ", i);
+
+    int Vetor[N];
+    for (int i = 0; i < N; i++) {
         scanf("%d", &Vetor[i]);
     }
-    for (int i = 0; i < N; i++)
-    {
-        if(Vetor[i] < menorValor){
+
+    int menorValor = Vetor[0];
+    int posicaoMenor = 0;
+
+    for (int i = 1; i < N; i++) {
+        if (Vetor[i] < menorValor) {
             menorValor = Vetor[i];
             posicaoMenor = i;
         }
     }
-    printf("O menor valor do vetor e: %d, e sua posicao %d.\n", menorValor, posicaoMenor);
+
+    printf("Menor valor: %d\n", menorValor);
+    printf("Posicao: %d\n", posicaoMenor);
+
+    return 0;
 }
