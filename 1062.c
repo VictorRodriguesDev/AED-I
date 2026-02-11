3b606b4d4808084292387119cde82fbacbcd4db2
Victor Henrique Rodrigues de Siqueira <victorsiq9125@gmail.com>
2026-02-11 11:46:06 -0300

    teste

--- 1062.c
+++ 1062.c
@@ -12,7 +12,7 @@ void push(cel **top, int x) {
     cel *p = malloc(sizeof(cel)); // cria nó
     p->value = x; // guarda valor
     p->next = *top; // liga nó
-    *top = p; 
+    *top = p; //guarda o topo
 }
 
 // desempilha elemento
